401e29ecd600eca067218210f8b4546acdfe9b35
kaptan58 <albaylogistik@hotmail.com>
2025-06-11 17:38:38 +0200

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,1 @@
-
-export default function Home() {
-  return (
-    <div>
-      <h1>🚗 VEIGO Admin Panel</h1>
-      <p>Yönetici paneli başarıyla çalışıyor.</p>
-    </div>
-  );
-}
+// example content
